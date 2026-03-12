27634caee35330fdd0aae68967909f9f8392c28f
DevShag <alishaguftadev@gmail.com>
2026-03-12 15:19:47 +0530

    data ingestion ran successfully

--- demo.py
+++ demo.py
@@ -3,11 +3,20 @@ from src.exception import MyException
 import sys
 #logging.debug("This is a debug message.")
 #logging.info("This is an info message")
-
+'''
 try:
     a = 1+ 'Z'
 except Exception as e:
     logging.info(e)
     raise MyException(e, sys) from e
+'''
+
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
+
+
 
 
